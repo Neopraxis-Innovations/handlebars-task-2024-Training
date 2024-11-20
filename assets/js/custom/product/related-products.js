028a5e3a3b60dbc241f0f6d5493f9671a777777e
Athul Krishna <athul.neopraxis.in>
2024-11-20 11:04:22 +0530

    feat:append the image to bundlebar code changed in js

--- assets/js/custom/product/related-products.js
+++ assets/js/custom/product/related-products.js
@@ -1,33 +1,33 @@
-// // Get all buttons with the class 'related-product-btn-2'
-// const buttons = document.querySelectorAll('.related-product-btn-2');
+// Get all buttons with the class 'related-product-btn-2'
+const buttons = document.querySelectorAll('.related-product-btn-2');
 
-// // Loop through each button and add a click event listener
-// buttons.forEach(function(button) {
-//   button.addEventListener('click', function() {
-//     // Get the 'data-image' attribute value from the clicked button
-//     const image = button.getAttribute('data-image');
-//     if (image) {
-//       console.log("--- image ", image);
+// Loop through each button and add a click event listener
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Get the 'data-image' attribute value from the clicked button
+    const image = button.getAttribute('data-image');
+    if (image) {
+      console.log("--- image ", image);
       
-//       // Create a new <img> element
-//       const img = document.createElement('img');
+      // Create a new <img> element
+      const img = document.createElement('img');
       
-//       // Set the src attribute to the image URL
-//       img.src = image;
-//       img.width=50;
-//       img.height=50;
+      // Set the src attribute to the image URL
+      img.src = image;
+      img.width=50;
+      img.height=50;
       
-//       // Append the image to the '.bundle-bar-coloumn-2' element
-//       const container = document.querySelector('.bundle-bar-coloumn-2');
-//       container.appendChild(img);
-//     } else {
-//       console.log("No image data found.");
-//     }
-//   });
-// });
-
-$(".related-product-btn-2").click(function () {
-    var image = $(this).attr("data-image");
-    console.log("--- image ", image);
-    $(".bundle-bar-coloumn-2").append(`<img src="${image}" style="width:50px; height:auto">`);
+      // Append the image to the '.bundle-bar-coloumn-2' element
+      const container = document.querySelector('.bundle-bar-coloumn-2');
+      container.appendChild(img);
+    } else {
+      console.log("No image data found.");
+    }
+  });
 });
+
+// $(".related-product-btn-2").click(function () {
+//     var image = $(this).attr("data-image");
+//     console.log("--- image ", image);
+//     $(".bundle-bar-coloumn-2").append(`<img src="${image}" style="width:50px; height:auto">`);
+// });
